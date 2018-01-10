9a638a2a479fd5d5520d3d6ebc19ea6f6bc28063
Selvakumar Subramanian <mydreams.info@gmail.com>
2018-01-10 14:34:06 +0530

    updated test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,43 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { UserService } from './service/user.service';
+import { AuthService } from './service/auth.service';
+import { BookService } from './service/book.service';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        SignupComponent,
+        BookDetailComponent,
+        BookEditComponent,
+        BookAddComponent
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [UserService, AuthService, BookService]
     }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   }));
 
 });
